908c69d8c7b630a767524b2df54cb5da10e8694b
Isak <isakulstrup@gmail.com>
2024-02-07 09:08:39 +0100

    extracted moveToClosed function added some comments

--- src/Engine/Path.elm
+++ src/Engine/Path.elm
@@ -10,6 +10,8 @@ type alias Node =
     }
 
 
+{-| Derive f cost of node
+-}
 f : Node -> Int
 f node =
     node.g + node.h
@@ -26,16 +28,32 @@ toList path =
     Dict.toList path.open ++ Dict.toList path.closed
 
 
+{-| Initialize path with start point in open list
+-}
 init : Point -> Path
 init from =
     Path (Dict.singleton from (Node 0 0)) Dict.empty
 
 
+{-| Find shortest path between two points using A\*
+-}
 pathfind : (Point -> Bool) -> Point -> Point -> Path
 pathfind canMove from to =
     init from |> step canMove from to
 
 
+{-| Move node from open to closed
+-}
+moveToClosed : ( Point, Node ) -> Path -> Path
+moveToClosed ( position, node ) path =
+    { path
+        | open = path.open |> Dict.remove position
+        , closed = path.closed |> Dict.insert position node
+    }
+
+
+{-| Main pathfinding loop
+-}
 step : (Point -> Bool) -> Point -> Point -> Path -> Path
 step canMove from to path =
     let
@@ -58,4 +76,9 @@ step canMove from to path =
                         |> List.filter (\p -> (Dict.member p path.closed |> not) && canMove p)
                         |> List.map (\p -> ( p, Node (Point.distance from p) (Point.distance to p) ))
             in
-            { path | open = Dict.union path.open (Dict.fromList neighbours) }
+            { path
+                | open =
+                    Dict.union path.open (Dict.fromList neighbours)
+            }
+                |> moveToClosed cheapest
+                |> step canMove from to
