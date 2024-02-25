70ca3598b5427d69a90f05e92ed2b17c973d80e9
Isak <isakulstrup@gmail.com>
2024-02-25 09:24:32 +0100

    removed editor stuff

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,6 @@ module Main exposing (Model, Msg, Tile, main)
 import Browser
 import Browser.Events
 import Dict exposing (Dict)
-import Editor exposing (EditorState, Tool(..))
 import Engine.Codec as Codec
 import Engine.Noise as Noise
 import Engine.Path as Path exposing (Path)
@@ -11,7 +10,6 @@ import Engine.Point as Point exposing (Point)
 import Engine.Render as Render exposing (Camera)
 import Html exposing (Html, main_)
 import Html.Attributes
-import Html.Events
 import Json.Decode exposing (Decoder)
 import Json.Encode
 import Ports
@@ -51,41 +49,6 @@ canMove tiles from to =
             False
 
 
-applyTool : Tool -> Point -> Dict Point Tile -> Dict Point Tile
-applyTool tool position tiles =
-    case tool of
-        Raise magnitude ->
-            Dict.update position
-                (\t ->
-                    case t of
-                        Just level ->
-                            Just (level + magnitude |> min magnitude)
-
-                        Nothing ->
-                            Just (-1 + magnitude)
-                )
-                tiles
-
-        Lower magnitude ->
-            Dict.update position
-                (Maybe.andThen
-                    (\level ->
-                        if (level - magnitude) < 0 then
-                            Nothing
-
-                        else
-                            Just (level - magnitude |> max 0)
-                    )
-                )
-                tiles
-
-        Level height ->
-            Dict.insert position height tiles
-
-        Pan ->
-            tiles
-
-
 
 -- MODEL
 
@@ -94,7 +57,6 @@ type alias Model =
     { tiles : Dict Point Tile
     , camera : Camera
     , hoverTile : Point
-    , editor : EditorState
     , mouseDown : Bool
     , viewPathDebug : Bool
     , seed : Seed
@@ -165,7 +127,6 @@ init mapJson =
         map
         (Render.newCamera |> Render.zoomCamera -0.7)
         ( 0, 0 )
-        Editor.init
         False
         False
         seed
@@ -180,7 +141,6 @@ init mapJson =
 type Msg
     = HoverHex Point
     | ClickedHex Point
-    | ClickedSidebarTool Tool
     | KeyPressed String
     | MouseDownChanged Bool
     | ClickedNewMap
@@ -190,52 +150,17 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         HoverHex pos ->
-            if model.editor.enabled && model.mouseDown then
-                let
-                    newMap : Dict Point Tile
-                    newMap =
-                        List.foldl (applyTool model.editor.selectedTool) model.tiles (pos :: Point.neighbours pos)
-                in
-                ( { model
-                    | tiles = newMap
-                    , hoverTile = pos
-                  }
-                , Ports.storeMap (Codec.encodeMap tileEncoder newMap)
-                )
-
-            else
-                ( { model
-                    | hoverTile = pos
-                  }
-                , Cmd.none
-                )
-
-        ClickedHex pos ->
-            if model.editor.enabled then
-                let
-                    newMap : Dict Point Tile
-                    newMap =
-                        List.foldl (applyTool model.editor.selectedTool) model.tiles (pos :: Point.neighbours pos)
-                in
-                ( { model | tiles = newMap }
-                , Ports.storeMap (Codec.encodeMap tileEncoder newMap)
-                )
-
-            else
-                ( model, Cmd.none )
-
-        ClickedSidebarTool tool ->
-            ( { model | editor = Editor.setTool tool model.editor }
+            ( { model
+                | hoverTile = pos
+              }
             , Cmd.none
             )
 
+        ClickedHex _ ->
+            ( model, Cmd.none )
+
         KeyPressed key ->
             case key of
-                " " ->
-                    ( { model | editor = Editor.toggle model.editor }
-                    , Cmd.none
-                    )
-
                 "a" ->
                     ( { model | camera = Render.moveCameraX -100 model.camera }
                     , Cmd.none
@@ -284,29 +209,6 @@ viewTile attrs ( position, _ ) =
              ]
                 ++ attrs
             )
-        ]
-
-
-svgClassList : List ( String, Bool ) -> Svg.Attribute msg
-svgClassList classes =
-    classes
-        |> List.filter Tuple.second
-        |> List.map Tuple.first
-        |> String.join " "
-        |> Svg.Attributes.class
-
-
-viewGhostTile : List Point -> Point -> Svg Msg
-viewGhostTile highlight position =
-    Render.viewHex
-        [ Render.hexTransform position
-        , Svg.Events.onMouseOver (HoverHex position)
-        , Svg.Events.onClick (ClickedHex position)
-        , Svg.Attributes.class "ghost-tile"
-        , Svg.Attributes.fill "transparent"
-        , Svg.Attributes.stroke "beige"
-        , Svg.Attributes.strokeWidth "1"
-        , svgClassList [ ( "highlight", List.member position highlight ) ]
         ]
 
 
@@ -364,53 +266,12 @@ gooFilter =
         ]
 
 
-viewToolPresetButton : Tool -> Tool -> Html Msg
-viewToolPresetButton selected tool =
-    Html.button
-        [ Html.Attributes.class ("preset-" ++ Editor.toolToString tool)
-        , Html.Attributes.class "tool-preset"
-        , Html.Attributes.classList [ ( "selected", selected == tool ) ]
-        , Html.Events.onClick (ClickedSidebarTool tool)
-        ]
-        [ Html.text (Editor.toolToString tool) ]
-
-
-viewEditor : Editor.EditorState -> Html Msg
-viewEditor editor =
-    Html.section
-        [ Html.Attributes.class "sidebar"
-        , Html.Attributes.classList [ ( "show-sidebar", editor.enabled ) ]
-        ]
-        [ Html.h1 [] [ Html.text "Tool" ]
-        , Html.div [ Html.Attributes.class "tool-presets" ]
-            [ viewToolPresetButton editor.selectedTool (Editor.Raise 1)
-            , viewToolPresetButton editor.selectedTool (Editor.Lower 1)
-            , viewToolPresetButton editor.selectedTool (Editor.Level 1)
-            , viewToolPresetButton editor.selectedTool Editor.Pan
-            ]
-        , Html.button [ Html.Events.onClick ClickedNewMap ] [ Html.text "new map" ]
-
-        -- , Html.div []
-        --     [ Html.h1 [] [ Html.text "Path debug" ]
-        --     , Html.input
-        --         [ Html.Attributes.type_ "checkbox"
-        --         , Html.Attributes.id "view-path-debug-checkbox"
-        --         , Html.Attributes.checked debug
-        --         , Html.Events.onClick (ToggledViewPathDebug (not debug))
-        --         ]
-        --         []
-        --     , Html.label [ Html.Attributes.for "view-path-debug-checkbox" ] [ Html.text "Show path debug" ]
-        --     ]
-        ]
-
-
 view : Model -> Html Msg
 view model =
     main_
         [ Html.Attributes.id "app"
         ]
-        [ viewEditor model.editor
-        , Render.svg
+        [ Render.svg
             [ Svg.Attributes.class "game-svg"
             , Svg.Events.onMouseDown (MouseDownChanged True)
             , Svg.Events.onMouseUp (MouseDownChanged False)
@@ -422,13 +283,7 @@ view model =
                 , Svg.Lazy.lazy2 viewTiles 1 model.tiles
                 , Svg.Lazy.lazy2 viewTiles 2 model.tiles
                 , Svg.Lazy.lazy2 viewTiles 3 model.tiles
-                , Svg.g []
-                    (if model.editor.enabled then
-                        List.map (viewGhostTile (Point.circle model.editor.brushRadius model.hoverTile)) (Render.square model.camera)
-
-                     else
-                        [ viewPath model.viewPathDebug model.tiles ( 0, 0 ) model.hoverTile ]
-                    )
+                , Svg.g [] [ viewPath model.viewPathDebug model.tiles ( 0, 0 ) model.hoverTile ]
                 ]
 
             -- Center screen indicator
