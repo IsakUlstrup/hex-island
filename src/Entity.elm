f1fdb4daff826e299f78e6ce289be7d8ae63ba4f
Isak <isakulstrup@gmail.com>
2024-10-21 10:50:15 +0200

    simplified entity

--- src/Entity.elm
+++ src/Entity.elm
@@ -1,4 +1,4 @@
-module Entity exposing (Entity, EntityData(..), move, new, pathfind, tickCooldown)
+module Entity exposing (Entity, move, new, pathfind, tickCooldown)
 
 import Engine.Path as Path
 import Engine.Point exposing (Point)
@@ -6,67 +6,30 @@ import Engine.Point exposing (Point)
 
 type alias Entity =
     { position : Point
-    , data : EntityData
-    }
-
-
-type EntityData
-    = Human HumanData
-    | Item
-    | Building
-
-
-new : Point -> Entity
-new position =
-    Entity position (Human (HumanData [] 0))
-
-
-updateData : (EntityData -> EntityData) -> Entity -> Entity
-updateData f entity =
-    { entity | data = f entity.data }
-
-
-
--- HUMAN
-
-
-type alias HumanData =
-    { path : List Point
+    , path : List Point
     , cooldown : Int
     }
 
 
+new : Point -> Entity
+new position =
+    Entity position [] 0
+
+
 tickCooldown : Float -> Entity -> Entity
 tickCooldown dt entity =
-    case entity.data of
-        Human humanData ->
-            updateData (always (Human { humanData | cooldown = humanData.cooldown - round dt |> max 0 })) entity
-
-        _ ->
-            entity
+    { entity | cooldown = entity.cooldown - round dt |> max 0 }
 
 
 move : Entity -> Entity
 move entity =
-    case entity.data of
-        Human humanData ->
-            case ( humanData.path, humanData.cooldown ) of
-                ( p :: ps, 0 ) ->
-                    { entity
-                        | position = p
-                    }
-                        |> updateData
-                            (always
-                                (Human
-                                    { humanData
-                                        | path = ps
-                                        , cooldown = 200
-                                    }
-                                )
-                            )
-
-                _ ->
-                    entity
+    case ( entity.path, entity.cooldown ) of
+        ( p :: ps, 0 ) ->
+            { entity
+                | position = p
+                , path = ps
+                , cooldown = 200
+            }
 
         _ ->
             entity
@@ -74,20 +37,15 @@ move entity =
 
 pathfind : (Point -> Point -> Bool) -> Point -> Entity -> Entity
 pathfind canMove target entity =
-    case entity.data of
-        Human humanData ->
-            let
-                path : Maybe (List Point)
-                path =
-                    Path.pathfind canMove entity.position target
-                        |> .path
-            in
-            case path of
-                Just validPath ->
-                    entity |> updateData (always (Human { humanData | path = validPath }))
+    let
+        path : Maybe (List Point)
+        path =
+            Path.pathfind canMove entity.position target
+                |> .path
+    in
+    case path of
+        Just validPath ->
+            { entity | path = validPath }
 
-                Nothing ->
-                    entity
-
-        _ ->
+        Nothing ->
             entity
